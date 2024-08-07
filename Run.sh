5a7df9029652ee6c1595365f3063a23593066028
Maksim Zinal <zinal@ydb.tech>
2024-08-07 14:57:59 +0300

    Примеры команд для запуска

--- Run.sh
+++ Run.sh
@@ -3,13 +3,13 @@
 # wget https://github.com/ydb-platform/ydb-jdbc-driver/releases/download/v2.2.1/ydb-jdbc-driver-shaded-2.2.1.jar
 # wget https://mycop1.website.yandexcloud.net/temp/ydb-join-test-1.0-SNAPSHOT.jar
 
-# ydb yql -s 'DROP TABLE dectest1'
+# ydb yql -s 'DROP TABLE ja; DROP TABLE jb; DROP TABLE jc;'
 
 # YDB_CONNECTION_STRING=grpcs://lb.etnphmvaf3ue1ub1srr6.ydb.mdb.yandexcloud.net:2135/ru-central1/b1g8skpblkos03malf3s/etnphmvaf3ue1ub1srr6
 # YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS=/Users/mzinal/Downloads/mzinal_sa.json
 
-YDB_CONNECTION_STRING=grpcs://zeit-1:2135/Domain0/zodak
-export YDB_CONNECTION_STRING
+#export YDB_CONNECTION_STRING=grpc://localhost:2136/Root/test
+export YDB_CONNECTION_STRING=grpcs://zeit-1:2135/Domain0/zodak
 
 YDB_USER='ydb-admin1@ldap'
 export YDB_USER
@@ -20,7 +20,4 @@ export YDB_PASSWORD
 YDB_SSL_ROOT_CERTIFICATES_FILE=/home/ubuntu/oldinstall/TLS/ca.crt
 export YDB_SSL_ROOT_CERTIFICATES_FILE
 
-java -classpath ydb-jdbc-driver-shaded-2.2.1.jar:ydb-join-test-1.0-SNAPSHOT.jar tech.ydb.samples.dectest.YdbDecimalTest
-
-# ydb yql -s 'SELECT * FROM dectest1'
-# ydb sql -s 'SELECT * FROM dectest1'
+java -classpath ydb-jdbc-driver-shaded-2.2.1.jar:ydb-join-test-1.0-SNAPSHOT.jar tech.ydb.samples.jointest.YdbJoinTest
